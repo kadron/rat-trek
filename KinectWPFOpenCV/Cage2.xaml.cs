4f0f0d2935df1b3b9ddcbc2fe627e88a1ba89af2
Cagri Sofuoglu <cagri.sofuoglu@gmail.com>
2014-05-21 08:54:06 +0300

    Cage drawRectangle implemented

--- KinectWPFOpenCV/Cage2.xaml.cs
+++ KinectWPFOpenCV/Cage2.xaml.cs
@@ -25,19 +25,19 @@ namespace KinectWPFOpenCV
     /// <summary>
     /// Interaction logic for Cage1.xaml
     /// </summary>
-    public partial class Cage1 : Window
+    public partial class Cage2 : Window
     {
         KinectSensor sensor;
         WriteableBitmap colorBitmap;
         DepthImagePixel[] depthPixels;
         byte[] colorPixels;
-        public Cage1()
+        public Cage2()
         {
             InitializeComponent();
-            this.Loaded += Cage1_Loaded;
+            this.Loaded += Cage2_Loaded;
             //this.cimg_cage1.Source = this.colorBitmap;
         }
-        void Cage1_Loaded(object sender, RoutedEventArgs e)
+        void Cage2_Loaded(object sender, RoutedEventArgs e)
         {
             foreach (var potentialSensor in KinectSensor.KinectSensors)
             {
@@ -49,12 +49,12 @@ namespace KinectWPFOpenCV
             }
             if (null != this.sensor)
             {
-                
+
                 this.sensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
                 this.sensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);
                 this.colorPixels = new byte[this.sensor.ColorStream.FramePixelDataLength];
                 this.colorBitmap = new WriteableBitmap(this.sensor.ColorStream.FrameWidth, this.sensor.ColorStream.FrameHeight, 96.0, 96.0, PixelFormats.Bgr32, null);
-                this.cimg_cage1.Source = this.colorBitmap;
+                this.cimg_cage2.Source = this.colorBitmap;
                 this.sensor.AllFramesReady += this.sensor_AllFramesReady;
                 try
                 {
@@ -80,23 +80,22 @@ namespace KinectWPFOpenCV
                         this.colorPixels,
                         this.colorBitmap.PixelWidth * sizeof(int),
                         0);
-                    
                     int sx = (int)this.sld_c1_sX.Value;
                     int sy = (int)this.sld_c1_sY.Value;
                     int dx = (int)this.sld_c1_dX.Value;
                     int dy = (int)this.sld_c1_dY.Value;
                     int w = 0;
                     int h = 0;
-                    if(dx >= sx)
+                    if (dx >= sx)
                         w = (dx - sx);
                     if (dy >= sy)
                         h = (dy - sy);
                     float cx = (float)sx + ((float)w) / 2;
                     float cy = (float)sy + ((float)h) / 2;
                     Image<Bgr, Byte> openCVImg = new Image<Bgr, byte>(colorBitmap.ToBitmap());
-                    MCvBox2D box = new MCvBox2D(new PointF(cx, cy),new SizeF(new PointF((float)w,(float)h)),0);
+                    MCvBox2D box = new MCvBox2D(new PointF(cx, cy), new SizeF(new PointF((float)w, (float)h)), 0);
                     openCVImg.Draw(box, new Bgr(System.Drawing.Color.Green), 4);
-                    this.cimg_cage1.Source = ImageHelpers.ToBitmapSource(openCVImg); 
+                    this.cimg_cage2.Source = ImageHelpers.ToBitmapSource(openCVImg);
                 }
             }
         }
